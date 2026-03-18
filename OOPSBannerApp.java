66c2b5999fb5437321cb310ae149388e19e5c901
Ankit1628 <ankitkasniya032@gmail.com>
2026-03-18 08:43:27 +0530

    Implemented UC5 - Refactor banner using method

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,21 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
 
-    public static void printBanner() {
-
-        String[] banner = {
-            "  ***      ***     *****     *****",
-            " *   *    *   *    *    *   *",
-            "*     *  *     *   *    *   *",
-            "*     *  *     *   *****     ****",
-            "*     *  *     *   *              *",
-            " *   *    *   *    *              *",
-            "  ***      ***     *         *****"
+        char[][] banner = {
+            {'*','*','*'},
+            {'*',' ','*'},
+            {'*',' ','*'},
+            {'*','*','*'}
         };
 
-        for(String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < banner.length; i++) {
+            for (int j = 0; j < banner[i].length; j++) {
+                System.out.print(banner[i][j]);
+            }
+            System.out.println();
         }
+
     }
+
 }
